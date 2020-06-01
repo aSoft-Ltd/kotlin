46f29855c392270829a0f4eaa157ae099a37c1d9
andylamax <andylamax@programmer.net>
2020-06-02 02:46:27 +0300

    Made the build reasonable excluding javafx and some android builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,26 @@ allprojects {
         maven(url = "https://dl.bintray.com/kotlin/kotlin-js-wrappers")
     }
     group = "tz.co.asoft"
-    version = "0.1.0-dev-04"
+    version = "0.1.0-dev-05"
 }
 
-listOf("primary", "secondary", "tertiary", "gradle-plugins", "ui-libs").forEach { group ->
+val groups = listOf("primary", "secondary", "tertiary", "gradle-plugins", "ui-libs")
+
+groups.forEach { group ->
     project.tasks.create("upload-$group") {
         project.file(group).listFiles().orEmpty().forEach { project ->
             dependsOn(":$group:${project.name}:publish")
+        }
+    }
+}
+
+subprojects {
+    if (name != "ui" && !groups.contains(name)) {
+        afterEvaluate {
+            val jarTasks = tasks.filter { it.name.contains("jar", true) }
+            tasks.create("sensitive-build") {
+                dependsOn(jarTasks)
+            }
         }
     }
 }
