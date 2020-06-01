438288bf0510cfa1af1491651c9d2a1f4754a852
andylamax <andylamax@programmer.net>
2020-06-01 19:27:53 +0300

    Seperated bintray uploads

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ allprojects {
         maven(url = "https://dl.bintray.com/kotlin/kotlin-js-wrappers")
     }
     group = "tz.co.asoft"
-    version = "0.1.0-dev-01"
+    version = "0.1.0-dev-02"
 }
 
 listOf("primary", "secondary", "tertiary", "gradle-plugins", "ui").forEach { group ->
