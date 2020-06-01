23f0136fad1d7f2eaaacb2ce4b4c0f5a6362ad1f
andylamax <andylamax@programmer.net>
2020-06-02 00:59:24 +0300

    Skipped building all together

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ allprojects {
     version = "0.1.0-dev-04"
 }
 
-listOf("primary", "secondary", "tertiary", "gradle-plugins", "ui").forEach { group ->
+listOf("primary", "secondary", "tertiary", "gradle-plugins", "ui-libs").forEach { group ->
     project.tasks.create("upload-$group") {
         project.file(group).listFiles().orEmpty().forEach { project ->
             dependsOn(":$group:${project.name}:publish")
