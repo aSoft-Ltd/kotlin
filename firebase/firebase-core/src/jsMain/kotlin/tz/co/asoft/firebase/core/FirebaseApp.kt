14013cdf4b11eddd393773d7bf6e5fc9e6e21f1a
andylamax <andylamax@programmer.net>
2020-06-25 01:58:26 +0300

    Fixed resolution issues with firebase-storage & firebase-firestore

--- firebase/firebase-core/src/jsMain/kotlin/tz/co/asoft/firebase/core/FirebaseApp.kt
+++ firebase/firebase-core/src/jsMain/kotlin/tz/co/asoft/firebase/core/FirebaseApp.kt
@@ -3,9 +3,6 @@ package tz.co.asoft.firebase.core
 actual external class FirebaseApp {
     val name: String
     val options: FirebaseOptions
-//    fun firestore() : FirebaseFirestore
-//    fun storage() : FirebaseStorage
-//    fun auth() : FirebaseAuth
 }
 
 actual val FirebaseApp.name: String
--- firebase/firebase-storage/src/commonMain/kotlin/tz/co/asoft/firebase/storage/StorageReference.kt
+++ firebase/firebase-storage/src/commonMain/kotlin/tz/co/asoft/firebase/storage/StorageReference.kt
@@ -1,6 +1,8 @@
 package tz.co.asoft.firebase.storage
 
 import tz.co.asoft.io.File
+import tz.co.asoft.io.FileRef
+import tz.co.asoft.io.name
 
 expect class StorageReference
 
@@ -8,6 +10,14 @@ expect val StorageReference.root: StorageReference
 expect fun StorageReference.child(path: String): StorageReference
 
 expect fun StorageReference.upload(file: File): UploadTask
+
+suspend fun StorageReference.push(file: File): FileRef {
+    upload(file).await()
+    return FileRef(
+        name = file.name,
+        url = downloadUrl() ?: throw Exception("Failed to get download url of ${file.name}")
+    )
+}
 
 expect suspend fun StorageReference.downloadUrl(): String?
 expect suspend fun StorageReference.remove()
