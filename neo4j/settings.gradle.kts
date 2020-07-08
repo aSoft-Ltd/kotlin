4355d51def35c627f53991f35909f20b62880de0
andylamax <andylamax@programmer.net>
2020-07-08 17:03:35 +0300

    Removed paging from neo4j at the moment

--- neo4j/settings.gradle.kts
+++ neo4j/settings.gradle.kts
@@ -8,3 +8,5 @@ pluginManagement {
 includeBuild("../build-src")
 include(":persist")
 project(":persist").projectDir = File("../persist")
+include(":paging-core")
+project(":paging-core").projectDir = File("../paging/paging-core")
