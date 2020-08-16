7efe666e81be146d1a3b3ea02e504211d97cbfc0
andylamax <andylamax@programmer.net>
2020-08-17 01:43:55 +0300

    Moving to dev-20

--- rest-api/rest-api-ktor/src/main/kotlin/tz/co/asoft/CORS.kt
+++ rest-api/rest-api-ktor/src/main/kotlin/tz/co/asoft/CORS.kt
@@ -7,16 +7,28 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.headersOf
 
+val HttpHeaders.XAuthToken get() = "X-Auth-Token"
+
 fun Application.installCORS() {
     install(CORS) {
         method(HttpMethod.Options)
         method(HttpMethod.Patch)
         method(HttpMethod.Delete)
-        header(HttpHeaders.AccessControlAllowHeaders)
-        header(HttpHeaders.AccessControlAllowMethods)
-        header(HttpHeaders.AccessControlAllowOrigin)
-        header(HttpHeaders.AccessControlRequestMethod)
-        header(HttpHeaders.AccessControlRequestHeaders)
+        headersOf(HttpHeaders.AccessControlAllowOrigin, "*")
+        headersOf(
+            HttpHeaders.AccessControlAllowMethods to listOf(
+                HttpMethod.Get.value,
+                HttpMethod.Post.value,
+                HttpMethod.Put.value,
+                HttpMethod.Patch.value,
+                HttpMethod.Delete.value,
+                HttpMethod.Head.value,
+                HttpMethod.Options.value
+            ),
+            HttpHeaders.AccessControlAllowHeaders to listOf(
+                HttpHeaders.Origin, HttpHeaders.ContentType, HttpHeaders.XAuthToken
+            )
+        )
         anyHost()
     }
 }
