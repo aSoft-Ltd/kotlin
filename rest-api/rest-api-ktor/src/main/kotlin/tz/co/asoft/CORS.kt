8c1bd26189ad007974fea6a0b12c540043637f4d
andylamax <andylamax@programmer.net>
2020-08-17 00:01:42 +0300

    Added headers again

--- rest-api/rest-api-ktor/src/main/kotlin/tz/co/asoft/CORS.kt
+++ rest-api/rest-api-ktor/src/main/kotlin/tz/co/asoft/CORS.kt
@@ -3,16 +3,20 @@ package tz.co.asoft
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CORS
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
+import io.ktor.http.headersOf
 
 fun Application.installCORS() {
     install(CORS) {
         method(HttpMethod.Options)
         method(HttpMethod.Patch)
         method(HttpMethod.Delete)
-        header("Access-Control-Allow-Origin")
-        header("Access-Control-Allow-Methods")
-        header("Access-Control-Allow-Headers")
+        header(HttpHeaders.AccessControlAllowHeaders)
+        header(HttpHeaders.AccessControlAllowMethods)
+        header(HttpHeaders.AccessControlAllowOrigin)
+        header(HttpHeaders.AccessControlRequestMethod)
+        header(HttpHeaders.AccessControlRequestHeaders)
         anyHost()
     }
 }
