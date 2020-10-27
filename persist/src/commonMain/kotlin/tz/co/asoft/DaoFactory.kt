8d77295ea92ca6ad66c02aaad0a465b3f790e1fa
andylamax <andylamax@programmer.net>
2020-10-27 04:14:58 +0300

    Added dao and service factories

--- persist/src/commonMain/kotlin/tz/co/asoft/DaoFactory.kt
+++ persist/src/commonMain/kotlin/tz/co/asoft/DaoFactory.kt
@@ -3,10 +3,11 @@ package tz.co.asoft
 import kotlinx.atomicfu.AtomicRef
 import kotlinx.atomicfu.atomic
 
-abstract class DaoFactory<T : Any> {
-    private val daoConfig: AtomicRef<T?> = atomic(null)
+abstract class DaoFactory<D : Any> {
+    private val daoConfig: AtomicRef<D?> = atomic(null)
     val dao get() = daoConfig.value ?: throw Exception("Dao ${this::class.simpleName} is not yet configured")
-    fun init(c: T) {
+    fun init(c: D) : D {
         daoConfig.value = c
+        return c
     }
 }
